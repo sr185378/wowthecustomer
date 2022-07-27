f6c30f1f97199fd93200fadf7d6eb5514fd93df0
Sachin Kumar Roy <sr185378@ncr.com>
2022-07-27 20:05:40 +0530

    Updated

--- src/Component/Data/MenuList.js
+++ src/Component/Data/MenuList.js
@@ -4,7 +4,7 @@ const Menulist=[
         "category":"drinks",
         "Price" : 10,
         "itemID" : 1,
-        "imageURL" : "https://www.lemonblossoms.com/wp-content/uploads/2020/12/Baklaka-Recipe-7.jpg",
+        "imageURL" : "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNvY2ElMjBjb2xhfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
         "loyaltyEnabled" : true,
         "Promoapplicable":	true
     },
@@ -13,7 +13,7 @@ const Menulist=[
         "category":"drinks",
         "Price" : 12,
         "itemID" : 2,
-        "imageURL" : "https://www.lemonblossoms.com/wp-content/uploads/2020/12/Baklaka-Recipe-7.jpg",
+        "imageURL" : "https://images.unsplash.com/photo-1625772299848-391b6a87d7b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3ByaXRlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
         "loyaltyEnabled" : true,
         "Promoapplicable":	false
     },
@@ -22,7 +22,7 @@ const Menulist=[
         "category":"Desserts",
         "Price" : 14,
         "itemID" : 3,
-        "imageURL" : "https://www.lemonblossoms.com/wp-content/uploads/2020/12/Baklaka-Recipe-7.jpg",
+        "imageURL" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHszRjHdf3lU_nC0G-JYQKBeGwpy5aZhPlsQ&usqp=CAU",
         "loyaltyEnabled" : true,
         "Promoapplicable":	false
     },
@@ -31,7 +31,7 @@ const Menulist=[
         "category":"Desserts",
         "Price" : 16,
         "itemID" : 4,
-        "imageURL" : "https://www.lemonblossoms.com/wp-content/uploads/2020/12/Baklaka-Recipe-7.jpg",
+        "imageURL" : "https://images.unsplash.com/photo-1572383672419-ab35444a6934?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXBwbGUlMjBwaWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
         "loyaltyEnabled" : true,
         "Promoapplicable":	true
     },
@@ -40,7 +40,7 @@ const Menulist=[
         "category":"Sides",
         "Price" : 18,
         "itemID" : 5,
-        "imageURL" : "https://www.lemonblossoms.com/wp-content/uploads/2020/12/Baklaka-Recipe-7.jpg",
+        "imageURL" : "https://images.unsplash.com/photo-1605262157780-8910063b2bf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZyaWVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
         "loyaltyEnabled" : true,
         "Promoapplicable":	true
     },
@@ -49,7 +49,7 @@ const Menulist=[
         "category":"Sides",
         "Price" : 20,
         "itemID" : 6,
-        "imageURL" : "https://www.lemonblossoms.com/wp-content/uploads/2020/12/Baklaka-Recipe-7.jpg",
+        "imageURL" : "https://images.unsplash.com/photo-1570466199012-e4e9ca168641?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hY2hvc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
         "loyaltyEnabled" : true,
         "Promoapplicable":	true
     },
@@ -58,7 +58,7 @@ const Menulist=[
         "category":"ItalianPizza",
         "Price" : 25,
         "itemID" : 7,
-        "imageURL" : "https://www.lemonblossoms.com/wp-content/uploads/2020/12/Baklaka-Recipe-7.jpg",
+        "imageURL" : "https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFyZ2hlcml0YSUyMHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
         "loyaltyEnabled" : true,
         "Promoapplicable":	false
     },
@@ -67,7 +67,7 @@ const Menulist=[
         "category":"ItalianPizza",
         "Price" : 25,
         "itemID" : 8,
-        "imageURL" : "https://www.lemonblossoms.com/wp-content/uploads/2020/12/Baklaka-Recipe-7.jpg",
+        "imageURL" : "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aGVhcnQlMjBvZiUyMHBhcmlzJTIwcGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
         "loyaltyEnabled" : false,
         "Promoapplicable":	true
     }
